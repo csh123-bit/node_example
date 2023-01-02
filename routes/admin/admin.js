f067516e4ad4d0078a0b46e6b068d5512ff4212f
test <cordial14@hanmail.net>
2023-01-02 22:38:36 +0900

    게시판 관리 추가 중

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -12,6 +12,7 @@ router.get('/login', (req, res)=>{
     res.render('../views/admina/login.html');
 });
 
+//게시판 관련 라우팅 분리 필요
 router.get('/board', async (req, res)=>{
     var boardList = await knex('board_config').select();
     res.render('../views/admin/board.html', {lists:boardList});
@@ -21,8 +22,37 @@ router.get('/add_board', (req, res)=>{
     res.render('../views/admin/addboard.html');
 });
 
+router.get('/add_board/:idx', async (req, res)=>{
+    var idx = req.params.idx;
+    var result = await knex('board_config').where('bod_idx',idx);
+    console.log(result);
+    res.render('../views/admin/addboard.html',{result:result[0]});
+});
+
 // 게시판 설정 저장
 router.post('/board_config', urlencodedParser, async (req, res)=>{
+    var board_code = req.body.board_code;
+    var board_skin = req.body.board_skin;
+    var board_per_page = req.body.board_page;
+    var board_name = req.body.board_name;
+
+    var isSuccess = await knex('board_config')
+    .insert({bod_code : board_code,
+            bod_skin : board_skin,
+            bod_per_page : board_per_page,
+            bod_name : board_name,
+            bod_created_at : new Date()}, ['bod_idx']);
+
+    console.log(isSuccess[0]);
+    if(isSuccess[0].bod_idx>0){
+        res.status(200).json("저장 되었습니다.");
+    }else{
+        res.status(200).json("저장실패.");
+    }
+
+});
+
+router.post('/board_config/:idx', urlencodedParser, async (req, res)=>{
     var board_code = req.body.board_code;
     var board_skin = req.body.board_skin;
     var board_per_page = req.body.board_page;
