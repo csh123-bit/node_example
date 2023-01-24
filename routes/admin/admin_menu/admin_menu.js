34c72178f5dbe10fe315f2a6133030ac8d094f4b
test <cordial14@hanmail.net>
2023-01-24 17:33:24 +0900

    관리자 화면 구현 중

---
+++ routes/admin/admin_menu/admin_menu.js
@@ -0,0 +1,14 @@
+module.exports = {
+    adminMenu:{
+        board:{
+            favicon:'',
+            title:'게시판관리',
+            url:'admin/board',
+        },
+        menu:{
+            favicon:'',
+            title:'메뉴관리',
+            url:'admin/menu',
+        },
+    }
+}
